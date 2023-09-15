139464a8d9a6d0769af767c2443a09f8f6b1d4b1
ptrjs <peterjose64@gmail.com>
2023-09-15 19:39:05 +0700

    revert

--- lib/module/favorite/view/favorite_view.dart
+++ lib/module/favorite/view/favorite_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:kaomoji_app/core.dart';
-import '../../../general_method/general_method.dart';
 import '../controller/favorite_controller.dart';
 
 class FavoriteView extends StatefulWidget {
@@ -10,6 +9,10 @@ class FavoriteView extends StatefulWidget {
     controller.view = this;
 
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Favorite"),
+        actions: const [],
+      ),
       body: SingleChildScrollView(
         child: Container(
           padding: const EdgeInsets.all(10.0),
@@ -27,7 +30,7 @@ class FavoriteView extends StatefulWidget {
                       : Colors.white;
                   return GestureDetector(
                     onTap: () => GeneralMethod().copyTextToClipboard(item),
-                    onDoubleTap: () => controller.removeFavorite(item),
+                    onDoubleTap: () => controller.removeFavorite(index),
                     child: Card(
                       color: cardColor,
                       child: ListTile(
--- lib/module/home/positive/joy/controller/joy_controller.dart
+++ lib/module/home/positive/joy/controller/joy_controller.dart
@@ -1,10 +1,11 @@
+import 'package:clipboard/clipboard.dart';
 import 'package:flutter/material.dart';
-
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:kaomoji_app/core.dart';
-
+import 'package:kaomoji_app/general_method/general_method.dart';
 import '../view/joy_view.dart';
 
-class JoyController extends State<JoyView> with AutomaticKeepAliveClientMixin {
+class JoyController extends State<JoyView> {
   static late JoyController instance;
   late JoyView view;
 
@@ -12,25 +13,13 @@ class JoyController extends State<JoyView> with AutomaticKeepAliveClientMixin {
   void initState() {
     instance = this;
     super.initState();
-    scrollController = ScrollController(initialScrollOffset: 0.0);
   }
 
   @override
-  void dispose() {
-    scrollController.dispose();
-    super.dispose();
-  }
+  void dispose() => super.dispose();
 
   @override
-  bool get wantKeepAlive => true;
-
-  @override
-  Widget build(BuildContext context) {
-    super.build(context);
-    return widget.build(context, this);
-  }
-
-  ScrollController scrollController = ScrollController();
+  Widget build(BuildContext context) => widget.build(context, this);
 
   List joy = [
     "(* ^ ω ^)",
